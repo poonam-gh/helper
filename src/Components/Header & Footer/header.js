6b96ef33e2338dec4dc416de73b56442cd21ed20
Himanshu Deulkar <97305434+HimanshuGD@users.noreply.github.com>
2024-01-27 23:51:57 +0530

    Delete src directory

--- src/Components/Header & Footer/header.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Heading({ text }) {
-    return (
-        <h1 className="mb-4 mt-4 text-[2.85rem] text-center font-extrabold leading-none tracking-normal text-gray-900 md:text-5xl lg:text-6xl">
-            {text}
-        </h1>
-    );
-}
